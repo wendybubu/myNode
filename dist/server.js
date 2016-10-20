55ce55be4c3e8b4db366f8cc738d42c46be4b262
wendy <94414547@qq.com>
2016-10-20 18:30:41 +0800

    1

---
+++ dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _http = require("http");
+
+var _http2 = _interopRequireDefault(_http);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_http2.default.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write('hello world!');
+    response.end();
+}).listen(8888);
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-var http = require('http');
+import http from 'http';
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write('hello world!');
     response.end();
 }).listen(8888);
